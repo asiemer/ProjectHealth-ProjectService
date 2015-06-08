6b3dc019cc8d29cb14f199234e9afab2560e9bda
Eric Fleming <eric-fleming18@hotmail.com>
2015-06-08 16:24:05 -0400

    Adding unit tests for a valid metrics.csv file

--- src/IntegrationTests/Services/MetricsProviderMockTests/when_creating_company_metric_from_MetricTestFile.cs
+++ src/IntegrationTests/Services/MetricsProviderMockTests/when_creating_company_metric_from_MetricTestFile.cs
@@ -9,19 +9,51 @@ namespace IntegrationTests.Services.MetricsProviderMockTests
     public class when_creating_company_metric_from_MetricTestFile
     {
         private MetricsProviderMock _metricsProviderMock;
+        private Metric[] _metrics;
 
         [SetUp]
         protected void When()
         {
             var fakeMetricsFileGetter = new FakeMetricsFileGetter("Metrics.csv");
             _metricsProviderMock = new MetricsProviderMock(fakeMetricsFileGetter);
+            _metrics = _metricsProviderMock.GetCompanyMetrics();
         }
 
         [Then]
         public void there_should_be_three_valid_metrics()
         {
-            var metrics = _metricsProviderMock.GetCompanyMetrics();
-            Assert.That(metrics.Count(), Is.EqualTo(3));
+            Assert.That(_metrics.Count(), Is.EqualTo(3));
+        }
+
+        [Then]
+        public void first_metrics_name_should_be_Metric_1()
+        {
+            Assert.That(_metrics.First().Name, Is.EqualTo("Metric 1"));
+        }
+
+        [Then]
+        public void first_metrics_IsDefault_should_be_true()
+        {
+            Assert.That(_metrics.First().IsDefault);
+        }
+
+        [Then]
+        public void last_metrics_weight_should_be_negative_50()
+        {
+            Assert.That(_metrics.Last().Weight, Is.EqualTo(-50));
+        }
+
+        [Then]
+
+        public void last_metrics_allowedAge_should_be_zero()
+        {
+            Assert.That(_metrics.Last().AllowedAge, Is.EqualTo(1));
+        }
+
+        [Then]
+        public void last_metrics_requiresAlert_should_be_zero()
+        {
+            Assert.That(!_metrics.Last().RequiresAlert);
         }
     }
 
