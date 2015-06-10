75e3e305fcaf89f2ecd2ff9b743029a8e3537c74
Eric Fleming <eric-fleming18@hotmail.com>
2015-06-10 15:16:14 -0400

    Added Project.Client console app for nservicebus

---
+++ src/Project.Client/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using NServiceBus;
+
+namespace Project.Client
+{
+    public class Program
+    {
+        static void Main()
+        {
+            var busConfiguration = new BusConfiguration();
+            busConfiguration.EndpointName("Projects.Project.Client");
+            busConfiguration.UseSerialization<JsonSerializer>();
+            busConfiguration.EnableInstallers();
+            busConfiguration.UsePersistence<InMemoryPersistence>();
+
+            using (IBus bus = Bus.Create(busConfiguration).Start())
+            {
+                SendOrder(bus);
+            }
+        }
+        static void SendOrder(IBus bus)
+        {
+            Console.WriteLine("Press 'Enter' to send a message. To exit press 'Ctrl + C'");
+
+            while (Console.ReadLine() != null)
+            {
+                Guid id = Guid.NewGuid();
+
+                var placeOrder = new PlaceOrder
+                {
+                    Product = "New shoes",
+                    Id = id
+                };
+                bus.Send("StepByStep.Ordering.Server", placeOrder);
+
+                Console.WriteLine("Sent a new PlaceOrder message with id: {0}", id.ToString("N"));
+            }
+        }
+    }
+}
